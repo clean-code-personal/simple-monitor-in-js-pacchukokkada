4e2f60222b67d92e9840f539c86af06d6ed08812
pacchukokkada <pacchukok@gmail.com>
2023-03-13 15:53:48 +0530

    simplified the code

--- bms-monitor.js
+++ bms-monitor.js
@@ -1,18 +1,4 @@
-let temperature = {'name':'temperature', 'max':45, 'min':0, 'metric':"°C" };
-let stateOfCharge = {'name':'state of charge','max':80, 'min':20, 'metric':"%" };
-let chargeRate = {'name':'charge rate', 'max':0.8, 'metric':"C"};
-
-function checkBattery(current_value,factor){
-
-    if( current_value < factor.min && 'min' in factor){
-        console.log(`${factor.name} is too low(${current_value}${factor.metric})`);
-        return false;
-    } else if(current_value > factor.max  && 'max' in factor) {
-        console.log(`${factor.name} is too high (${current_value}${factor.metric})`);
-        return false;
-    }
-    return true;
-}
+const { temperature, stateOfCharge, chargeRate, checkBattery} = require('./batteryChecker')
 
 function batteryIsOk(temperature_value, soc_value, charge_rate_value) {
     console.log("+++++++++++");
